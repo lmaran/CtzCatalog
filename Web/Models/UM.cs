e519292568af6370938542e60d830fdf313d9e2d
Lucian M <lucian.maran@outlook.com>
2015-01-23 20:00:32 +0200

    some UM modifications

--- Web/Models/UM.cs
+++ Web/Models/UM.cs
@@ -9,22 +9,22 @@ namespace Web.Models
     public class UM : Entity
     {
         public string Name { get; set; }
-        public string PluralName { get; set; }
+        //public string PluralName { get; set; }
 
         // Base: m, kg, buc
         // Aggregate: rola=f(m), bara=f(m), cutie=f(buc, m, kg)
         public string Type { get; set; } // Base or Aggregate
         
         // for Aggregate Types only
-        public List<ShortUMItem> BaseUMItems { get; set; }
-        public ShortUMItem DefaultBaseUM { get; set; }
-        public Decimal? DefaultValue { get; set; }
-        public List<Decimal?> Values { get; set; }
+        public List<ShortUMItem> BaseUMItems { get; set; } // first = default
+        //public ShortUMItem DefaultBaseUM { get; set; } 
+        public List<Decimal?> Values { get; set; } //first = default
+        //public Decimal? DefaultValue { get; set; }
     }
 
     public class ShortUMItem : Entity
     {
         public string Name { get; set; }
-        public string PluralName { get; set; }
+        //public string PluralName { get; set; }
     }
 }
