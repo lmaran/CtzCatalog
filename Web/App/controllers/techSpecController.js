cd2896a6087c1721bfc0770b3aac9ac3b9fa67f3
Lucian M <lucian.maran@outlook.com>
2015-02-15 21:12:23 +0200

    major clean up for techSpec

--- Web/App/controllers/techSpecController.js
+++ Web/App/controllers/techSpecController.js
@@ -1,45 +1,17 @@
 ﻿app.controller('techSpecController', ['$scope', '$window', '$route', 'techSpecService', '$location', function ($scope, $window, $route, techSpecService, $location) {
     $scope.isEditMode = $route.current.isEditMode;
-    $scope.isFocusOnOptions = false;
     $scope.isFocusOnName = $scope.isEditMode ? false : true;
 
     $scope.techSpec = {};
 
     $scope.dotObject = {};
-    $scope.dotObject.isRenameMode = {};
-
 
     $scope.selectedSpecItem = {};
     $scope.newSpecItem = {};
+
     $scope.selectedSection = {};
+    $scope.newSection = {};
 
-
-    $scope.optionBtnAreVisible = false;
-
-    $scope.initAddSpecItemMode = function () {
-        $scope.dotObject.isVisibleAddNewItem = true;
-        $scope.selectedIndex = -1;
-        $scope.newSpecItem.name = '';
-        $scope.dotObject.isFocusOnAddItem = true;
-        $scope.isItemInEditMode = false;
-    }
-
-    $scope.initAddSectionMode = function () {
-        $scope.dotObject.isVisibleAddNewSection = true;
-        //$scope.selectedIndex = -1;
-        $scope.newSectionName = '';
-        $scope.dotObject.isFocusOnAddSection = true;
-        $scope.isSectionInEditMode = false;
-    }
-
-    $scope.initRenameSpecItemMode = function (currentSpecItem) {
-        //$scope.dotObject.renamedItamName = currentSpecItem.name;
-        $scope.selectedSpecItem.name = currentSpecItem.name;
-
-        $scope.dotObject.specItemName = currentSpecItem.name;
-        $scope.dotObject.isFocusOnRenameItem = true;
-
-    }
 
     if ($scope.isEditMode) {
         $scope.pageTitle = 'Edit techSpec';
@@ -65,16 +37,199 @@
         });
     }
 
-    $scope.addSpecItemOnEnter = function (currentSection, newSpecItem, e) {
+
+    // =============================== Section ops. ================================================
+
+    // ---------------- Add Section ---------------------
+
+    $scope.initAddModeForSection = function () {
+        $scope.newSection = {};
+        $scope.dotObject.isFocusOnAddSection = true;
+
+        // open this window and close the others
+        $scope.selectedSection.name = null;
+        $scope.selectedSpecItem.name = null;
+        $scope.dotObject.isVisibleAddNewSection = true;
+        $scope.dotObject.isVisibleAddNewSpecItem = false;
+    }
+
+    $scope.addSection = function (newSection) {
+        if (!isValidSection(newSection)) return false;
+
+        $scope.techSpec.sections.push(newSection);
+
+        $scope.dotObject.isVisibleAddNewSection = false;
+    }
+
+    $scope.addSectionOnEnter = function (newSection, e) {
         if (e.which == 13) { //enter key
             e.preventDefault();
             e.stopPropagation();
-            $scope.addSpecItem(currentSection, newSpecItem);
+            $scope.addSection(newSection);
         };
     }
 
-    $scope.addSpecItem = function (currentSection, newSpecItem) {
-        var items = currentSection.specItems;
+    // ---------------- Rename Section ------------------
+
+    $scope.initRenameModeForSection = function (section) {
+        $scope.newSection = angular.copy(section); // deep copy (or use _.cloneDeep(section))
+
+        $scope.dotObject.isFocusOnRenameSection = true;
+
+        // open this window and close the others
+        $scope.selectedSection.name = section.name;
+        $scope.selectedSpecItem.name = null;
+        $scope.dotObject.isVisibleAddNewSection = false;
+        $scope.dotObject.isVisibleAddNewSpecItem = false;
+    };
+
+    $scope.renameSection = function (section, newSection) {
+        if (newSection.name == section.name) {
+            //close 'rename' window
+            $scope.selectedSection.name = null;
+            return true;
+        };
+
+        if (!isValidSection(newSection)) {
+            $scope.dotObject.isFocusOnRenameSection = true;
+            return;
+        };
+
+        // update section name
+        section.name = newSection.name;
+
+        //close 'rename' section
+        $scope.selectedSection.name = null;
+    };
+
+    // ---------------- Delete Section ------------------
+
+    $scope.removeSection = function (section) {
+        _.remove($scope.techSpec.sections, function (currentItem) {
+            return currentItem.name == section.name;
+        });
+    };
+
+    // ---------------- Validate Section ----------------
+
+    function isValidSection(newSection) {
+        var items = $scope.techSpec.sections;
+
+        if (newSection.name == '') {
+            alert("Enter a value and then press the button!");
+            $scope.dotObject.isFocusOnAddSection = true; // not necessary when Enter key is used
+            return false;
+        };
+
+        if (_.findIndex(items, { 'name': newSection.name }) != -1) {
+            alert('This value already exists: ' + newSection.name);
+            $scope.dotObject.isFocusOnAddSection = true; // not necessary when Enter key is used
+            return false;
+        };
+
+        return true;
+    }
+
+    // ---------------- SortableOptions Section & Toogle ----
+
+    $scope.sortableOptionsForSection = {
+        accept: function (sourceItemHandleScope, destSortableScope) {
+            //return true;
+            // do not allow moving between specItems (parent) and options (child)
+            return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
+        },
+        itemMoved: function (event) { },
+        orderChanged: function (event) { },
+        dragStart: function (event) {
+            // collapse any expanded accordion items
+            $scope.dotObject.expandedSectionName = null;
+        }
+        //containment: '#board'
+    };
+
+    $scope.toogleSection = function (sectionName) {
+        $scope.dotObject.expandedSectionName = $scope.dotObject.expandedSectionName == sectionName ? null : sectionName;
+    };
+
+
+
+    // =============================== SpecItem ops. ===============================================
+
+    // ---------------- Add SpecItem --------------------
+
+    $scope.initAddModeForSpecItem = function () {
+        $scope.newSpecItem = {};
+        $scope.dotObject.isFocusOnAddItem = true;
+
+        // open this window and close the others
+        $scope.selectedSection.name = null;
+        $scope.selectedSpecItem.name = null;
+        $scope.dotObject.isVisibleAddNewSection = false;
+        $scope.dotObject.isVisibleAddNewSpecItem = true;
+    }
+
+    $scope.addSpecItem = function (section, newSpecItem) {
+        if (!isValidSpecItem(section, newSpecItem)) return false;
+
+        section.specItems.push(newSpecItem);
+
+        $scope.dotObject.isVisibleAddNewSpecItem = false;
+    }
+
+    $scope.addSpecItemOnEnter = function (section, newSpecItem, e) {
+        if (e.which == 13) { //enter key
+            e.preventDefault();
+            e.stopPropagation();
+            $scope.addSpecItem(section, newSpecItem);
+        };
+    }
+
+    // ---------------- Rename SpecItem -----------------
+
+    $scope.initRenameModeForSpecItem = function (currentSpecItem) {
+        $scope.newSpecItem = angular.copy(currentSpecItem); // deep copy (or use _.cloneDeep(currentSpecItem))
+
+        $scope.dotObject.isFocusOnRenameSpecItem = true;
+
+        // open this window and close the others
+        $scope.selectedSection.name = null;
+        $scope.selectedSpecItem.name = currentSpecItem.name;
+        $scope.dotObject.isVisibleAddNewSection = false;
+        $scope.dotObject.isVisibleAddNewSpecItem = false;
+
+    };
+
+    $scope.renameSpecItem = function (section, specItem, newSpecItem) {
+        if (newSpecItem.name == specItem.name) {
+            //close 'rename' window
+            $scope.selectedSpecItem.name = null;
+            return true;
+        };
+
+        if (!isValidSpecItem(section, newSpecItem)) {
+            $scope.dotObject.isFocusOnRenameSection = true;
+            return;
+        };
+
+        // update specItem name
+        specItem.name = newSpecItem.name;
+
+        //close 'rename' section
+        $scope.selectedSpecItem.name = null;
+    };
+
+    // ---------------- Delete SpecItem -----------------
+
+    $scope.removeSpecItem = function (section, specItem) {
+        _.remove(section.specItems, function (currentItem) {
+            return currentItem.name == specItem.name;
+        });
+    };
+
+    // ---------------- Validate SpecItem ---------------
+
+    function isValidSpecItem(section, newSpecItem) {
+        var items = section.specItems;
 
         if (newSpecItem.name == '') {
             alert("Enter a value and then press the button!");
@@ -88,38 +243,38 @@
             return;
         }
 
-        items.push({ name: newSpecItem .name});
-
-        $scope.dotObject.isVisibleAddNewItem = false;
-        //$scope.newSpecItem = {}; //reset value
+        return true;
     }
 
-    $scope.addSection = function (sectionName) {
-        var items = $scope.techSpec.sections;
+    // ---------------- SortableOptions & Toogle SpecItem ---
 
-        if (sectionName == '') {
-            alert("Enter a value and then press the button!");
-            $scope.dotObject.isFocusOnAddSection = true; // not necessary when Enter key is used
-            return;
+    $scope.sortableOptionsForSpecItem = {
+        accept: function (sourceItemHandleScope, destSortableScope) {
+            //return true;
+            // do not allow moving between specItems (parent) and options (child)
+            return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
+        },
+        itemMoved: function (event) { },
+        orderChanged: function (event) { },
+        dragStart: function (event) {
+            // collapse any expanded accordion items
+            $scope.dotObject.expandedItemName = null;
         }
+        //containment: '#board'
+    };
 
-        if (_.findIndex(items, { 'name': sectionName }) != -1) {
-            alert('This value already exists: ' + sectionName);
-            $scope.dotObject.isFocusOnAddSection = true; // not necessary when Enter key is used
-            return;
-        }
+    $scope.toogleSpecItem = function (specItemName) {
+        $scope.dotObject.expandedSpecItemName = $scope.dotObject.expandedSpecItemName == specItemName ? null : specItemName;
+    };
 
-        items.push({ name: sectionName, specItems: [{options:[], defaultOptions:[]}]});
 
-        $scope.dotObject.isVisibleAddNewSection = false;
-        //$scope.newSpecItem = {}; //reset value
-    }
 
-    $scope.renameSpecItem = function (newSpecItem) {
-        newSpecItem.name = $scope.dotObject.specItemName;
-        //close 'rename' section
-        $scope.dotObject.renamedItamName = null;
-    }
+
+
+
+    // ================ MainForm (TechSpec) ops. ===============================================
+
+    // ---------------- Create TechSpec -----------------
 
     $scope.create = function (form) {
         $scope.submitted = true;
@@ -158,233 +313,5 @@
         $window.history.back();
     }
 
-    $scope.addOptionOnEnter = function (event) {
-        if (event.which == 13) { //enter key
-            event.preventDefault();
-            event.stopPropagation();
-            $scope.addOption();
-        };
-    }
-
-    $scope.addOption = function () {
-        if ($scope.newOptionValue) {
-            if (!$scope.techSpec.options) $scope.techSpec.options = [];
-            $scope.techSpec.options.push({ name: $scope.newOptionValue });
-        } else {
-            alert("Enter a value and then press the button!");
-            return;
-        };
-        
-        $scope.newOptionValue = undefined;
-        $scope.isFocusOnOptions = true;
-
-        // remove $$haskKey property from objects
-        // met.1 - use angular.copy: --> $scope.techSpec.options = angular.copy($scope.techSpec.options);
-        // met.2 - alert(angular.toJson($scope.techSpec.options));
-        // met.3 - use 'track by' in ng-repeat (I use that method because it is faster: http://www.codelord.net/2014/04/15/improving-ng-repeat-performance-with-track-by/)
-        // and don't have to clean up the object later on
-
-    };
-
-    $scope.removeSection = function (currentSection) {
-        _.remove($scope.techSpec.sections, function (currentItem) {
-            return currentItem.name == currentSection.name;
-        });
-    };
-
-    $scope.removeSpecItem = function (currentSection, specItem) {
-        _.remove(currentSection.specItems, function (currentItem) {
-            return currentItem.name == specItem.name;
-        });
-    };
-
-    $scope.removeSpecItemOption = function (specItem, option) {
-        _.remove(specItem.options, function (currentItem) {
-            return currentItem.value == option.value;
-        });
-    };
-
-    $scope.optionUp = function (oldIdx, option, e) {
-        // to not expand the panel at the end of action
-        if (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        };
-
-        var newIdx = oldIdx - 1, tmp;
-        var options = $scope.techSpec.options;
-
-        var optionsLength = options.length;
-
-        if (oldIdx > 0) {
-            tmp = options[newIdx];
-            options[newIdx] = options[oldIdx];
-            options[oldIdx] = tmp;
-        } else { // oldIndex is first position
-            newIdx = optionsLength - 1; // circular list
-            tmp = options[oldIdx];
-
-            // move all remaining options one position up
-            for (var i = 1; i <= optionsLength; i++) {
-                options[i - 1] = options[i];
-            };
-            options[newIdx] = tmp;
-        }
-        // options is just another reference to $scope.techSpec.options;
-        // so we don't have to switch back (e.g. $scope.techSpec.options = options)
-    }
-
-    $scope.optionDown = function (oldIdx, option, e) {
-        // to not expand the panel at the end of action
-        if (e) {
-            e.preventDefault();
-            e.stopPropagation();
-        };
-
-        var newIdx = oldIdx + 1, tmp;
-        var options = $scope.techSpec.options;
-
-        var optionsLength = options.length;
-
-        if (oldIdx < optionsLength - 1) {
-            tmp = options[newIdx];
-            options[newIdx] = options[oldIdx];
-            options[oldIdx] = tmp;
-        } else { // oldIndex is last position
-            newIdx = 0; // circular list
-            tmp = options[oldIdx];
-
-            // move all remaining options one position down
-            for (var i = (optionsLength - 1); i > 0; i--) {
-                options[i] = options[i-1];
-            };
-            options[newIdx] = tmp;
-        }
-        // options is just another reference to $scope.techSpec.options;
-        // so we don't have to switch back (e.g. $scope.techSpec.options = options)
-    }
-
-    // helper functions
-    // get the index of selected object in array (objects with one level depth, selected by one of its property)
-    function getIndex(data, propertyName, propertyValue) {
-        var idx = -1;
-        for (i = 0; i < data.length; i++) {
-            if (data[i][propertyName] === propertyValue) {
-                idx = i;
-                break;
-            };
-        };
-        return idx;
-
-        // met. 2 (shorter but requires full scan of array; IE > 8)
-        //return data.map(function (e) { return e[propertyName]; }).indexOf(propertyValue);
-    }
-
-    $scope.sortableOptions1 = {
-        accept: function (sourceItemHandleScope, destSortableScope) {
-            //return true;
-            // do not allow moving between specItems (parent) and options (child)
-            return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
-        },
-        itemMoved: function (event) { },
-        orderChanged: function (event) { },
-        dragStart: function (event) {
-            // collapse any expanded accordion items
-            //$scope.dotObject.expandedItemName = null;
-        }
-        //containment: '#board'
-    };
-
-    $scope.sortableOptions = {
-        accept: function (sourceItemHandleScope, destSortableScope) {
-            //return true;
-            // do not allow moving between specItems (parent) and options (child)
-            return sourceItemHandleScope.itemScope.sortableScope.$id === destSortableScope.$id;
-        },
-        itemMoved: function (event) {},
-        orderChanged: function (event) { },
-        dragStart: function (event) {
-            // collapse any expanded accordion items
-            $scope.dotObject.expandedItemName = null;
-        },
-        containment: '#board'
-    };
-
-
-    $scope.toogleCollapse = function (specItemName) {
-
-
-        $scope.dotObject.expandedItemName = $scope.dotObject.expandedItemName == specItemName ? null : specItemName;
-
-        //$event.preventDefault();
-        //$event.stopPropagation();
-    };
-
-
-    $scope.toogleSection = function (sectionName) {
-        $scope.dotObject.expandedSectionName = $scope.dotObject.expandedSectionName == sectionName ? null : sectionName;
-    };
-
-    $scope.status = {
-        isopen: false
-    };
-
-    $scope.toggled = function (open) {
-        //$log.log('Dropdown is now: ', open);
-
-        // collapse any expanded accordion items
-        $scope.dotObject.expandedItemName = null;
-    };
-
-    $scope.toggleDropdown = function ($event) {
-        $event.preventDefault();
-        $event.stopPropagation();
-        $scope.status.isopen = !$scope.status.isopen;
-    };
 
 }]);
-
-//http://fdietz.github.io/recipes-with-angular-js/common-user-interface-patterns/editing-text-in-place-using-html5-content-editable.html
-app.directive("contenteditable", function () {
-    return {
-        restrict: "A",
-        require: "ngModel",
-        link: function (scope, element, attrs, ngModel) {
-
-            function read() {
-                ngModel.$setViewValue(element.html());
-            }
-
-            ngModel.$render = function () {
-                element.html(ngModel.$viewValue || "");
-            };
-
-            element.bind("blur keyup change", function () {
-                scope.$apply(read);
-            });
-        }
-    };
-});
-
-// http://stackoverflow.com/a/17586334/2726725
-app.directive('splitArray', function () {
-    return {
-        restrict: 'A',
-        require: 'ngModel',
-        link: function (scope, element, attr, ngModel) {
-
-            function fromUser(text) {
-                return text.split("\n");
-            }
-
-            function toUser(array) {
-                if (array == undefined)
-                    return null;
-                return array.join("\n");
-            }
-
-            ngModel.$parsers.push(fromUser);
-            ngModel.$formatters.push(toUser);
-        }
-    };
-});
